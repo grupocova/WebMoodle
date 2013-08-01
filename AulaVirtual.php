4e9b57ba0a1d7bfbe76556b5ce5d5f26f9a18087
unknown <maribel.condori@FIA-WEBMASTER.lim.upeu.edu.pe>
2013-08-01 10:57:15 -0500

    Archivos iniciales

---
+++ AulaVirtual.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Aula Virtual | FIA</title>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.min.css" />
+<link rel="stylesheet" type="text/css" href="css/prettify.css" />
+<link rel="stylesheet" type="text/css" href="css/docs.css" />
+</head>
+
+<body>
+<div class="navbar navbar-inverse navbar-fixed-top">
+  <div class="navbar-inner">
+    <div class="container">      
+      <a class="brand" href="#">Aula Virtual</a>
+      <div class="nav-collapse collapse">
+        <ul class="nav">
+          <li><a href="#">FIA</a></li>
+          <li><a href="#">UPeU</a></li>
+        </ul>        
+      </div>      
+    </div>
+  </div>
+</div>
+
+
+<!-- Header -->
+<header>
+  
+</header>
+
+<!-- slider-->
+<div class="container">
+
+</div>
+
+<!-- Principal -->
+<div class="container">
+<div class="row-fluid">
+  <div class="span3"> 
+  	<h3>AULA Virtual</h3>
+    <h4>Facultad de Ingeniería y Arquitectura</h4>
+	<form action="http://localhost/fia/login/index.php" method="post" id="login">
+      <div class="input-prepend">
+        <span class="add-on"><i class="icon-user"></i></span>
+        <input class="span10" id="username" type="text" placeholder="username" name="username">
+      </div>
+      <br>    
+      <div class="input-prepend">
+        <span class="add-on"><i class="icon-lock"></i></span>
+        <input class="span10" id="password" type="password" placeholder="password" name="password">
+      </div>             
+       <div class="control-group">
+        <div class="controls">
+          <label class="checkbox">
+            <input type="checkbox" name="rememberusername" id="rememberusername" value="1"> Recordarme
+          </label>
+          <button type="submit" class="btn">Ingresar</button>
+        </div>
+      </div>       
+    </form>
+  </div>
+  <div class="span9 hero-unit">
+   
+    <h1>Heading</h1>
+    <p>Tagline</p>
+    <p>
+    <a class="btn btn-primary btn-large">
+    Learn more
+    </a>
+    </p>
+             
+  </div>
+</div>
+</div>
+
+<!-- footer -->
+<footer class="footer">
+ <div class="container">
+        <p>Designed and built with all the love in the world by <a href="http://twitter.com/mdo" target="_blank">@mdo</a> and <a href="http://twitter.com/fat" target="_blank">@fat</a>.</p>
+        <p>Code licensed under <a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">Apache License v2.0</a>, documentation under <a href="http://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a>.</p>
+        <p><a href="http://glyphicons.com">Glyphicons Free</a> licensed under <a href="http://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a>.</p>
+        <ul class="footer-links">
+          <li><a href="http://blog.getbootstrap.com">Blog</a></li>
+          <li class="muted">&middot;</li>
+          <li><a href="https://github.com/twitter/bootstrap/issues?state=open">Issues</a></li>
+          <li class="muted">&middot;</li>
+          <li><a href="https://github.com/twitter/bootstrap/blob/master/CHANGELOG.md">Changelog</a></li>
+        </ul>
+      </div>
+</footer>
+<script src="js/jquery.js"></script>
+  <script src="js/bootstrap-transition.js"></script>
+  <script src="js/bootstrap-alert.js"></script>
+  <script src="js/bootstrap-modal.js"></script>
+  <script src="js/bootstrap-dropdown.js"></script>
+  <script src="js/bootstrap-scrollspy.js"></script>
+  <script src="js/bootstrap-tab.js"></script>
+  <script src="js/bootstrap-tooltip.js"></script>
+  <script src="js/bootstrap-popover.js"></script>
+  <script src="js/bootstrap-button.js"></script>
+  <script src="js/bootstrap-collapse.js"></script>
+  <script src="js/bootstrap-carousel.js"></script>
+  <script src="js/bootstrap-typeahead.js"></script>
+  <script src="js/bootstrap-affix.js"></script>
+  <script src="js/holder.js"></script>
+  <script src="js/prettify.js"></script>
+  <script src="js/application.js"></script>
+</body>
+</html>
