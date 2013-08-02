7cb6bdfd6eb76796b1ec7c0ac7275012bee7b2ef
grupocova <marib.mcs@gmail.com>
2013-08-02 08:12:24 -0500

    Avance Diseno 02/08/2013

--- AulaVirtual.php
+++ AulaVirtual.php
@@ -3,118 +3,117 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Aula Virtual | FIA</title>
-<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
-<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.min.css" />
-<link rel="stylesheet" type="text/css" href="css/prettify.css" />
-<link rel="stylesheet" type="text/css" href="css/docs.css" />
-<link rel="stylesheet" type="text/css" href="css/EBase.css" />
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css" />
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
+<!--<link rel="stylesheet" type="text/css" href="css/prettify.css" />-->
+<!--<link rel="stylesheet" type="text/css" href="css/docs.css" />-->
+<link rel="stylesheet" type="text/css" href="css/Style.css" />
+<link rel="stylesheet" type="text/css" href="Bootstrap/css/bootstrap-glyphicons.css" />
 </head>
 
 <body>
-<div class="navbar navbar-inverse navbar-fixed-top">
-  <div class="navbar-inner">
-    <div class="container">      
-      <a class="brand" href="#">Aula Virtual</a>
-      <div class="nav-collapse collapse">
-        <ul class="nav">
-          <li><a href="#">FIA</a></li>
-          <li><a href="#">UPeU</a></li>
-        </ul>        
-      </div>      
-    </div>
+
+<div class="navbar navbar-fixed-top navprin">
+  <div class="container">
+    <a class="navbar-brand" href="#">Project name</a>
+    <div class="nav-collapse collapse">
+      <ul class="nav navbar-nav">
+        <li><a href="#">UPeU</a></li>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Cursos</a></li>
+        <li><a href="#">Docentes</a></li>
+        <li><a href="#">Biblioteca Virtual</a></li>           
+      </ul>
+    </div><!--/.nav-collapse -->
   </div>
 </div>
-
 
 <!-- Header -->
 <header>
   
 </header>
 
-<!-- slider-->
-<div class="container">
-
-</div>
 
 <!-- Principal -->
 <div class="container">
-<div class="row-fluid well">
-  <ul>
-    <li class="span3">
+<div class="row">
+  <!--<ul>
+    <li class="col-lg-6">
       <a href="#">
         <img data-src="holder.js/260x100" alt="">
       </a>
     </li>
-    <li class="span3 offset6">
+    <li class="col-lg-6">
       <a href="#">
         <img data-src="holder.js/260x100" alt="">
       </a>
     </li>
-  </ul>
+  </ul>-->
 </div>
-<div class="row-fluid well">
-  <div class="span3"> 
-  	<h3>AULA Virtual</h3>
-    <h4>Facultad de Ingeniería y Arquitectura</h4>
-	<form action="http://localhost/fia/login/index.php" method="post" id="login">
-      <div class="input-prepend">
-        <span class="add-on"><i class="icon-user"></i></span>
-        <input class="span10" id="username" type="text" placeholder="username" name="username">
-      </div>
-      <br>    
-      <div class="input-prepend">
-        <span class="add-on"><i class="icon-lock"></i></span>
-        <input class="span10" id="password" type="password" placeholder="password" name="password">
-      </div>             
-       <div class="control-group">
-        <div class="controls">
-          <label class="checkbox">
+
+<div class="row">
+  <div class="col-lg-3 panel"> 
+  	<div class="panel-heading contenedor">
+      <h3 class="panel-title">Bienvenido</h3>
+    </div>
+    <form>
+      <fieldset>
+        <legend>Iniciar Sesion</legend>
+        <div class="form-group input-group">
+          <span class="input-group-addon glyphicon glyphicon-user"></span>
+          <input type="text" class="form-control" id="username" name="username" placeholder="Usuario">
+        </div>
+        <div class="form-group input-group">
+          <span class="input-group-addon glyphicon glyphicon-lock"></span>
+          <input type="password" class="form-control" id="password" name="password" placeholder="Contraseña">
+        </div>
+        <div class="checkbox">
+          <label>
             <input type="checkbox" name="rememberusername" id="rememberusername" value="1"> Recordarme
           </label>
-          <button type="submit" class="btn">Ingresar</button>
         </div>
-      </div>       
+        <button type="submit" class="btn btn-default">Ingresar</button>
+      </fieldset>
     </form>
+      
   </div>
   
-  <div class="span9 hero-unit">   
+  <div class="col-lg-7 hero-unit">   
     <h1>Heading</h1>
     <p>Tagline</p>
     <p><a class="btn btn-primary btn-large">Learn more</a></p>             
+  </div>
+  
+  <div class="col-lg-2 panel contenedor">   
+    <ul class="nav nav-pills nav-stacked">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Profile</a></li>
+      <li><a href="#">Messages</a></li>
+    </ul>             
   </div>
 </div>
 </div>
 
 <!-- footer -->
-<footer>
-  <div class="container row-fluid">    
-    <div class="icosocial span4 offset4">
-      <p class="text-center">© Facultad de Ingeniería y Arquitectura <br />2013 UPeU</p>
+<footer class="footer">
+  <div class="container"> 
+  <div class="row">   
+    <div class="icosocial col-lg-12 panel">
+      <p>©2013 Universidad Peruana Uni&oacute;n - Facultad de Ingeniería y Arquitectura</p>
+      <br />
       <ul>        
-      <li><a href="#" id="gog"><span>Google +</span></a></li>
+      <li><a href="#" id="face"><span>Facebook</span></a></li>
+      <li><a href="#" id="twit"><span>Twitter</span></a></li>
       <li><a href="#" id="you"><span>Youtube</span></a></li>
-      <li><a href="#" id="twit"><span>Twitter</span></a></li>        
-      <li><a href="#" id="face"><span>Facebook</span></a></li>        
-      </ul>
-    </div>      
+      <li><a href="#" id="gog"><span>Google +</span></a></li>
+      </ul>      
+    </div>    
+  </div>     
   </div>
 </footer>
 <script src="js/jquery.js"></script>
-  <script src="js/bootstrap-transition.js"></script>
-  <script src="js/bootstrap-alert.js"></script>
-  <script src="js/bootstrap-modal.js"></script>
-  <script src="js/bootstrap-dropdown.js"></script>
-  <script src="js/bootstrap-scrollspy.js"></script>
-  <script src="js/bootstrap-tab.js"></script>
-  <script src="js/bootstrap-tooltip.js"></script>
-  <script src="js/bootstrap-popover.js"></script>
-  <script src="js/bootstrap-button.js"></script>
-  <script src="js/bootstrap-collapse.js"></script>
-  <script src="js/bootstrap-carousel.js"></script>
-  <script src="js/bootstrap-typeahead.js"></script>
-  <script src="js/bootstrap-affix.js"></script>
   <script src="js/holder.js"></script>
-  <script src="js/prettify.js"></script>
-  <script src="js/application.js"></script>
+  <script src="Bootstrap/js/bootstrap.js"></script>
+  <script src="Bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
