abfb429e713a83be06423320ba1646e3d0c34ece
unknown <maribel.condori@FIA-WEBMASTER.lim.upeu.edu.pe>
2013-08-07 15:53:53 -0500

    Diseno inforamcion de carreras, paginas docente.php y curso.php

---
+++ Docente.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Aula Virtual | FIA</title>
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css" />
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
+<!--<link rel="stylesheet" type="text/css" href="css/prettify.css" />-->
+<!--<link rel="stylesheet" type="text/css" href="css/docs.css" />-->
+<link rel="stylesheet" type="text/css" href="css/Style.css" />
+<link rel="stylesheet" type="text/css" href="Bootstrap/css/bootstrap-glyphicons.css" />
+</head>
+
+<body>
+<div class="body-container"> 
+
+  <!-- Navegacion Superior -->
+  <div class="navbar navbar-fixed-top navprin">
+  <div class="container">
+    <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".md-navbar-collapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="AulaVirtual.php"><!-- <img src="photos/minilogo_AV.png"  alt=""> -->Aula Virtual</a>
+    <div class="nav-collapse collapse md-navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li><a href="http://www.upeu.edu.pe/" target="_blank">UPeU</a></li>
+        <li><a href="Cursos.php">Cursos</a></li>
+        <li><a href="Docente.php">Docentes</a></li>
+        <li><a href="#">Biblioteca Virtual</a></li>           
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div>
+  </div><!-- Fin Navegacion Superior -->
+
+  <!-- Contenido -->
+  <div class="container">
+  <div class="row encabezado">
+    <div class="col-lg-12">  
+      <a href="#" class="logo logo-left">
+        <img src="photos/logo_upeu.png"  alt="">
+      </a>
+      <a href="#" class="logo logo-right">
+      <img src="photos/logo_AV.png"  alt="">
+      </a>
+    </div>
+  </div>
+
+  <!-- Inf. Cursos -->
+  <div class="row">
+    <div class="col-lg-10 col-offset-1 panel" id="cont-info">
+      <div class"row">
+        <div class="col-lg-7 cont-info-list">
+          <div class="page-header">
+            <h2>Docentes <small>Aula Virtual</small></h2>
+          </div>
+          <div class="media">
+            <a class="pull-left" href="#">
+            <img class="media-object" data-src="holder.js/90x90">
+            </a>
+          <div class="media-body">
+            <h4 class="media-heading">Ing. Ambiental</h4>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo.
+          </div>
+          </div>
+
+          <div class="media">
+            <a class="pull-left" href="#">
+            <img class="media-object" data-src="holder.js/90x90">
+            </a>
+          <div class="media-body">
+            <h4 class="media-heading">Ing. Ambiental</h4>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo.
+          </div>
+          </div>
+
+          <div class="media">
+            <a class="pull-left" href="#">
+            <img class="media-object" data-src="holder.js/90x90">
+            </a>
+          <div class="media-body">
+            <h4 class="media-heading">Ing. Ambiental</h4>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo.
+          </div>
+          </div>
+        </div>
+
+        <div class="col-lg-5">
+          <a href="#" class="thumbnail">
+            <img data-src="holder.js/100%x430" alt="">
+          </a>
+        </div>
+      </div>      
+    </div>
+  </div><!-- Fin Inf. Cursos -->
+
+  <!--Iconos Sociales-->
+  <div class="row">
+    <div class="col-lg-12 icosocial" id="links-sociales">
+      <ul>        
+        <li><a href="#" id="face"><span>Facebook</span></a></li>
+        <li><a href="#" id="twit"><span>Twitter</span></a></li>
+        <li><a href="#" id="you"><span>Youtube</span></a></li>
+        <li><a href="#" id="gog"><span>Google +</span></a></li>
+      </ul> 
+    </div>
+  </div><!--Fin Iconos Sociales-->
+
+  </div><!--Fin Contenido -->
+
+  <footer class="footer">
+    <div>
+      <p>©2013 Universidad Peruana Uni&oacute;n - Facultad de Ingeniería y Arquitectura<br />
+        Altura Km. 19.5 Carretera Central, Ñaña-Lima <br />
+        Telf.: 01-6186300 - Fax 01-6186-339 <br />
+        Correo: webmaster.fia@upeu.edu.pe <br />
+        Lima - Perú</p>
+    </div>      
+  </footer> 
+
+</div>
+
+<script src="js/jquery.js"></script>
+  <script src="js/holder.js"></script>
+  <script src="Bootstrap/js/bootstrap.js"></script>
+  <script src="Bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
