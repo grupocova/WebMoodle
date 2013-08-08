4b25951b78ff8057705593f881aeaa407a9cf1f4
unknown <maribel.condori@FIA-WEBMASTER.lim.upeu.edu.pe>
2013-08-08 09:42:03 -0500

    respaurar archivos completos

---
+++ index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Aula Virtual | FIA</title>
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css" />
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
+<!--<link rel="stylesheet" type="text/css" href="css/prettify.css" />-->
+<!--<link rel="stylesheet" type="text/css" href="css/docs.css" />-->
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-glyphicons.css" />
+</head>
+
+<body>
+<div class="body-container"> 
+
+  <!-- Navegacion Superior -->
+  <div class="navbar navbar-fixed-top navprin">
+  <div class="container">
+    <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".md-navbar-collapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="index.php"><!-- <img src="photos/minilogo_AV.png"  alt=""> -->Aula Virtual</a>
+    <div class="nav-collapse collapse md-navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li><a href="http://www.upeu.edu.pe/" target="_blank">UPeU</a></li>
+        <li><a href="cursos.php">Cursos</a></li>
+        <li><a href="docente.php">Docentes</a></li>
+        <li><a href="#">Biblioteca Virtual</a></li>           
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div>
+  </div><!-- Fin Navegacion Superior -->
+
+  <!-- Contenido -->
+  <div class="container">
+  <div class="row encabezado">
+    <div class="col-lg-12">  
+      <a href="#" class="logo logo-left">
+        <img src="photos/logo_upeu.png"  alt="">
+      </a>
+      <a href="#" class="logo logo-right">
+      <img src="photos/logo_av.png"  alt="">
+      </a>
+    </div>
+  </div>
+
+  <!-- Principal -->
+  <div class="row">    
+    <div class="col-lg-5 col-offset-1">
+      
+      <!--Contenido Informacion-->
+      <div class="panel " id="cont-info">
+        <ul id="myTab" class="nav nav-tabs md-tab-nav">
+          <li class="active"><a href="#arq" data-toggle="tab">Arquitectura</a></li>
+          <li><a href="#ali" data-toggle="tab">Alimentos</a></li>
+          <li><a href="#amb" data-toggle="tab">Ambiental</a></li>   
+          <li><a href="#civ" data-toggle="tab">Civil</a></li> 
+          <li><a href="#sis" data-toggle="tab">Sistemas</a></li>      
+        </ul>
+
+      <div id="myTabContent" class="tab-content md-tab-cont">
+        <!--Arquitectura-->
+        <div class="tab-pane fade in active" id="arq">         
+          <div class="media">
+            <a class="pull-left" href="#">
+            <img class="media-object" data-src="holder.js/150x180">
+            </a>
+          <div class="media-body">
+            <h4 class="media-heading">Arquitectura</h4>
+            <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo.Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.</p>
+          </div>
+          </div>
+        </div><!--Fin Arquitectura-->
+
+        <!--Aliemntos-->
+        <div class="tab-pane fade" id="ali">
+          <div class="media">
+            <a class="pull-left" href="#">
+            <img class="media-object" data-src="holder.js/150x180">
+            </a>
+          <div class="media-body">
+            <h4 class="media-heading">Ing. de Alimentos</h4>
+            <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo.Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.</p>
+          </div>
+          </div>
+        </div><!--Fin Aliemntos-->
+
+        <!--Ambiental-->
+        <div class="tab-pane fade" id="amb">
+          <div class="media">
+            <a class="pull-left" href="#">
+            <img class="media-object" data-src="holder.js/150x180">
+            </a>
+          <div class="media-body">
+            <h4 class="media-heading">Ing. Ambiental</h4>
+            <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo.Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.</p>           
+          </div>
+          </div>
+        </div><!--Fin Ambiental-->
+
+        <!--Civil-->
+        <div class="tab-pane fade" id="civ">
+          <div class="media">
+            <a class="pull-left" href="#">
+            <img class="media-object" data-src="holder.js/150x180">
+            </a>
+          <div class="media-body">
+            <h4 class="media-heading">Ing. Civil</h4>
+            <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo.Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.</p>
+          </div>
+          </div>
+        </div><!--Fin Civil-->
+
+        <!--Sistemas-->
+        <div class="tab-pane fade" id="sis">
+          <div class="media">
+            <a class="pull-left" href="#">
+            <img class="media-object" data-src="holder.js/150x180">
+            </a>
+          <div class="media-body">
+            <h4 class="media-heading">Ing. de Sistemas</h4>
+            <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo.Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.</p>
+          </div>
+          </div>
+        </div><!--Fin Sistemas-->
+      </div>
+      </div><!--Contenido Informacion-->
+
+    </div>
+    
+    <!--Login-->
+    <div class="col-lg-3 panel cont" id="cont-login">
+      <div class="panel-heading head">
+        <h3 class="panel-title">Bienvenido</h3>
+      </div>
+      <form method="post" action="http://posgrado.upeu.edu.pe/cova/upeu_virtual/login/index.php" target="_top">
+        <fieldset>
+          <legend>Iniciar Sesion</legend>
+          <div class="form-group input-group">
+            <span class="input-group-addon glyphicon glyphicon-user"></span>
+            <input type="text" class="form-control" id="username" name="username" placeholder="Usuario">
+          </div>
+          <div class="form-group input-group">
+            <span class="input-group-addon glyphicon glyphicon-lock"></span>
+            <input type="password" class="form-control" id="password" name="password" placeholder="Contraseña">
+          </div>
+          <div class="checkbox">
+          <label>
+            <input type="checkbox" name="rememberusername" id="rememberusername" value="1"> Recordarme
+          </label>
+          </div>
+          <button type="submit" class="btn btn-default">Ingresar</button>
+        </fieldset>
+      </form>
+    </div> <!--Fin Login-->
+
+    <!--Opciones-->
+    <div class="col-lg-2 panel cont" id="cont-login-op"> 
+      <ul class="list-group">      
+        <li class="list-group-item"><a href="#" class="iconos"><div id="ico-reg"></div>Registrar</a></li>
+        <li class="list-group-item"><a href="#" class="iconos"><div id="ico-inf"></div>Olvido contraseña?</a></li>
+        <li class="list-group-item"><a href="#" class="iconos"><div id="ico-unlock"></div>Información</a></li>
+      </ul>  
+    </div> <!-- Fin Opciones-->
+    
+  </div> <!--Fin  Principal --> 
+
+  <!--Iconos Sociales-->
+  <div class="row">
+    <div class="col-lg-12 icosocial" id="links-sociales">
+      <ul>        
+        <li><a href="#" id="face"><span>Facebook</span></a></li>
+        <li><a href="#" id="twit"><span>Twitter</span></a></li>
+        <li><a href="#" id="you"><span>Youtube</span></a></li>
+        <li><a href="#" id="gog"><span>Google +</span></a></li>
+      </ul> 
+    </div>
+  </div><!--Fin Iconos Sociales-->
+
+  </div><!--Fin Contenido -->
+
+  <footer class="footer">
+    <div>
+      <p>©2013 Universidad Peruana Uni&oacute;n - Facultad de Ingeniería y Arquitectura<br />
+        Altura Km. 19.5 Carretera Central, Ñaña-Lima <br />
+        Telf.: 01-6186300 - Fax 01-6186-339 <br />
+        Correo: webmaster.fia@upeu.edu.pe <br />
+        Lima - Perú</p>
+    </div>      
+  </footer> 
+
+</div>
+
+<script src="js/jquery.js"></script>
+  <script src="js/holder.js"></script>
+  <script src="bootstrap/js/bootstrap.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
